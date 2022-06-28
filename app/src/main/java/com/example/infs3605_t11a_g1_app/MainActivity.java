b9c0cbb50f2db7a37c1f1f3f7f1990965d748040
fionawahidin <90660117+fionawahidin@users.noreply.github.com>
2022-06-28 13:05:34 +1000

    Initial commit

---
+++ app/src/main/java/com/example/infs3605_t11a_g1_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.infs3605_t11a_g1_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
